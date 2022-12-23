1f3aea28282b7c16ef60da71225e9f184106b235
BharathSharath <103111105+BharathSharath@users.noreply.github.com>
2022-12-23 13:43:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello ssh agent plugin is working');
+  res.send('Hello ssh agent plugin is working and webhook also working');
 });
 
 app.listen(PORT, HOST);
